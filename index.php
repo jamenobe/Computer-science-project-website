f4f1b8e23a6eca6ceddfc821fe727039263e3956
jamie <jammienobbble@gmail.com>
2018-11-05 15:20:02 +0000

    Website

--- index.php
+++ index.php
@@ -1,7 +1,65 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jame
- * Date: 17/09/2018
- * Time: 3:28 PM
- */
+<!DOCTYPE html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style sheet.css"/>
+<body>
+
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.html">PC Builder</a>
+        </div>
+
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="index.html">Start</a></li>
+            <li><a href="CPU page.html">CPU</a></li>
+            <li><a href="#">Motherboard</a></li>
+            <li><a href="#">Case</a></li>
+            <li><a href="#">RAM</a></li>
+            <li><a href="#">GPU</a></li>
+            <li><a href="#">PSU</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col-sm-2" style=""></div>
+        <div class="col-sm-8 col-centered" style="">
+
+            <h1 >PC Spec Builder</h1>
+
+        </div>
+    </div>
+    <br/>
+    <div class="row">
+
+        <div class="col-sm-3" style=""></div>
+        <div class="col-sm-6" style="" align="center">
+
+            <p>
+                Lorem ipsum dolor sit amet, penatibus sit blandit. Vel sed amet class nec qui, ac nec, eleifend nam, dictum vulputate duis sit id est, elit ultricies sit turpis ac scelerisque. Sapien molestie porta suspendisse ante, vestibulum litora, lacinia et et tempus, dignissim vel cras dolor risus rutrum fusce. Dignissim et ultrices molestie sit sollicitudin mollis. Id rhoncus convallis dui et commodo, non dolor dolor vestibulum magna tellus aliquam, magna a sit enim nam, tellus tristique tempor vivamus mi vitae, pede ut imperdiet. Nec mauris odio dignissim ut sociis mauris. Malesuada massa libero morbi porta, non lectus pharetra tristique fermentum vestibulum quis, sit sed augue quam vel amet lacus, non sapien sem erat felis, in eu ac at ut elit. Accumsan feugiat quam vestibulum tristique tristique condimentum, ornare nam enim. Imperdiet dui netus turpis turpis, augue felis et purus turpis, praesent blandit proin integer praesent tincidunt.
+            </p>
+
+        </div>
+
+    </div>
+    <br/>
+    <div class="row">
+
+        <div class="col-sm-2" style=""></div>
+        <div class="col-sm-8" style="" align="center">
+
+            <a class="btn btn-primary" href="CPU page.html" role="button">Start</a>
+
+        </div>
+
+    </div>
+</div>
+
+
+</body>
+
+</head>
